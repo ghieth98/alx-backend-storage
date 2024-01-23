547d2796d8e9e1f0902672f556f26c4b1c4aafb1
hossam ghieth <hossamghieth@gmail.com>
2024-01-23 21:22:07 +0200

    Create 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module that provides some stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    collection = client.logs.nginx
+
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for met in methods:
+        print("\tmethod {}: {}".format(
+            met, collection.count_documents({"method": met})))
+    print("{} status check".format(
+        collection.count_documents({"path": "/status"})))
+
+    print("IPs:")
+    ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in ips:
+        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
