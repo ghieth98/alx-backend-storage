f3cef54c9b2c7801a7cec45d53b3200098a45bdc
hossam ghieth <hossamghieth@gmail.com>
2024-04-09 16:06:19 +0200

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3
-"""A module for caching data and tracking the number of accesses."""
-from functools import wraps
-from typing import Callable
-
+""" implements a get_page function
+"""
 import redis
 import requests
 
-redis_store = redis.Redis()
+count = 0
+cache = redis.Redis()
 
 
-def data_cacher(func: Callable[[str], str]) -> Callable[[str], str]:
-    """A decorator that caches data and tracks the number of accesses."""
-
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        """The wrapper function for caching the output."""
-        if not redis_store.exists(f"count:{url}"):
-            redis_store.set(f"count:{url}", 0)
-        redis_store.incr(f"count:{url}")
-
-        result = redis_store.get(f"result:{url}")
-        if result is not None:
-            return result.decode("utf-8")
-
-        result = func(url)
-        redis_store.setex(f"result:{url}", 10, result)
-        return result
-
-    return wrapper
-
-
-@data_cacher
 def get_page(url: str) -> str:
-    """Fetch the content of a URL."""
-    return requests.get(url).text
+    """Obtains the HTML content of a particular URL and returns it.
+    Tracks how many times the URL was accessed and store this
+    count in a Redis cache.
+    """
+    cache.set(f"cached:{url}", count)
+    response = requests.get(url)
+    cache.incr(f"count:{url}")
+    cache.setex(f"count:{url}", 10, cache.get(f"cached:{url}"))
+    return response.text
