6b2acd235e257b3a93269282a6b1313a0c4b97c6
hossam ghieth <hossamghieth@gmail.com>
2024-04-09 15:53:37 +0200

    update

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,48 +1,28 @@
 #!/usr/bin/env python3
-""" Module that provides some stats about Nginx logs stored in MongoDB """
+""" Python script that provides some stats about Nginx logs stored in MongoDB
+    Database: logs
+    Collection: nginx
+ """
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    collection = client.logs.nginx
 
-def main() -> None:
-    """ Provides some stats about Nginx logs stored in MongoDB """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(method, collection.count_documents(
+            {"method": method})))
+    print("{} status check".format(
+        collection.count_documents({"path": "/status"})))
 
-    total_logs = collection.count_documents({})
-
-    method_counts = {
-        'GET': collection.count_documents({"method": "GET"}),
-        'POST': collection.count_documents({"method": "POST"}),
-        'PUT': collection.count_documents({"method": "PUT"}),
-        'PATCH': collection.count_documents({"method": "PATCH"}),
-        'DELETE': collection.count_documents({"method": "DELETE"})
-    }
-
-    status_check = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-
-    # Get the top 10 most present IPs
-    ip_counts = collection.aggregate([
+    print("IPs:")
+    ips = collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
-
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check} status check")
-
-    print("IPs:")
-    for ip in ip_counts:
-        print(f"\t{ip['_id']}: {ip['count']}")
-
-    client.close()
-
-
-if __name__ == "__main__":
-    main()
+    for ip in ips:
+        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
